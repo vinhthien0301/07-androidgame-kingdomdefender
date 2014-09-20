fc1c8694bc51338c7d2122370a32977d75b37341
vinh.thien0301 <vinh.thien0301@gmail.com>
2014-09-20 11:24:23 +0700

    init project

---
+++ KingdomDefender/gen/com/techstorm/androidgame/kingdomdefender/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.techstorm.androidgame.kingdomdefender;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
